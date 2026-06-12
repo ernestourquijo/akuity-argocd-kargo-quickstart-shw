004079995449f913b3f07306caa8444aab94a351
Daniel Hoang <danielvanhoang@gmail.com>
2026-06-12 09:28:47 -0500

    Normalize architecture handling in download-cli.sh for Kargo releases

--- download-cli.sh
+++ download-cli.sh
@@ -8,6 +8,11 @@ fi
 version=$(basename $(curl -Ls -o /dev/null -w %{url_effective} https://github.com/akuity/kargo/releases/latest))
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m)
+# Normalize architecture to match Kargo release asset names (amd64/arm64).
+case "${arch}" in
+  x86_64) arch=amd64 ;;
+  aarch64) arch=arm64 ;;
+esac
 download_url=https://github.com/akuity/kargo/releases/download/${version}/kargo-${os}-${arch}
 
 curl -L -o ${1} ${download_url}
